5c1c372b18cedaa61a80e6841a2fb295a468a45e
tannertracht <31142056+tannertracht@users.noreply.github.com>
2018-05-14 12:30:59 -0600

    Updated UI

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 # The Class WindowClass is inheriting from wx.Frame
 class WindowClass(wx.Frame):
+    filepath = None
+    threshhold = 25
+    kernelsize = 15
 
     def __init__(self, *args, **kwargs):
         # Super has something to do with inheritance
@@ -10,9 +13,9 @@ class WindowClass(wx.Frame):
         self.SetSize(800, 600)
         self.basicGUI()
 
+
     def basicGUI(self):
-        global panel
-        panel = wx.Panel(self)
+        self.panel = wx.Panel(self)
         # Define menuBar and set it to a MenuBar type
         menubar = wx.MenuBar()
         # Define fileButton and set it to a Menu type
@@ -21,24 +24,32 @@ class WindowClass(wx.Frame):
         exititem = filebutton.Append(wx.ID_EXIT, 'Exit', 'status msg...')
         # Attach fileButton to menuBar
         menubar.Append(filebutton, 'File')
-        # Define imagebutton
-        imagebutton = wx.Button(panel, -1, 'Analyze Image')
+        # Define buttons and stuff
+        analyzebutton = wx.Button(self.panel, -1, 'Analyze Image', wx.Point(0, 25))
+        imagebutton = wx.Button(self.panel, -1, 'Choose Image')
+        thresholdinput = wx.TextCtrl(self.panel, 5, pos=wx.Point(0, 55), style=wx.TE_PROCESS_ENTER)
+        kernelsizeinput = wx.TextCtrl(self.panel, -1, pos=wx.Point(0, 85), style=wx.TE_PROCESS_ENTER)
 
         # Set the frame's MenuBar to the menuBar we've created
         self.SetMenuBar(menubar)
         # Search for a call originating from exitItem in EVT_MENU and call self.Quit
         self.Bind(wx.EVT_MENU, self.quit, exititem)
 
-        # Bind imagebutton to function
-        self.Bind(wx.EVT_BUTTON, self.AnalyzeImage, imagebutton)
+        # Bind Buttons and Stuff
+        self.Bind(wx.EVT_BUTTON, self.AnalyzeImage, analyzebutton)
+        self.Bind(wx.EVT_BUTTON, self.filedialog, imagebutton)
+        self.Bind(wx.EVT_TEXT_ENTER, self.updatethreshold, thresholdinput)
+        self.Bind(wx.EVT_TEXT_ENTER, self.updatekernel, kernelsizeinput)
+        wx.StaticText(self.panel, -1, 'Threshold value (Default 25)', wx.Point(115, 60))
+        wx.StaticText(self.panel, -1, 'Kernel Matrix Size (Default 15)', wx.Point(115, 90))
 
-        self.SetTitle('Peach Blossom Detection')
+        self.SetTitle('Control Panel')
         self.Show(True)
 
     def quit(self, e):
         self.Close()
 
-    def filedialog(self):
+    def filedialog(self, e):
         with wx.FileDialog(self, "Open File",
                            style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
 
@@ -46,31 +57,33 @@ class WindowClass(wx.Frame):
                 return  # the user changed their mind
 
             # Set pathname to the file path of the image
-            pathname = fileDialog.GetPath()
-            return pathname
+            self.filepath = fileDialog.GetPath()
+            wx.StaticText(self.panel, -1, self.filepath, wx.Point(105, 5))
 
 
     def AnalyzeImage(self, e):
-        pathname = self.filedialog()
+        pathname = self.filepath
         # Load visible image with
         visibleimg = cv2.imread(pathname, 1)
         # Split the image into the 3 bands
         b, g, r = cv2.split(visibleimg)
         # Subtract R image from G image to highlight blossoms
         rminusg = cv2.subtract(r, g)
-        # bminusg = cv2.subtract(b, g) not currently using this in calculations
+        bminusg = cv2.subtract(b, g)  # not currently using this in calculations
         # Convert image to binary image with thresholding
-        th, binaryimg = cv2.threshold(rminusg, 25, 255, cv2.THRESH_BINARY);
+        th, binaryimg = cv2.threshold(rminusg, self.threshhold, 255, cv2.THRESH_BINARY);
         # Kernel used to denoise image, used in morphology functions
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (self.kernelsize, self.kernelsize))
         # Denoise using morphology opening (Eroding then Dilating)
         denoiseimg = cv2.morphologyEx(binaryimg, cv2.MORPH_OPEN, kernel)
-        # Label the Pixel Groups. Returns 4 matrixes in labels
+        # Label the Pixel Groups. Returns 4 matrixes inside the 'labels' array
         labels = cv2.connectedComponentsWithStats(denoiseimg, 4, cv2.CV_32S)
         # The fourth value of labels is a matrix where each row is a label
         # and each column is that label's x coordinate and y coordinate respectively
         centroids = labels[3]
         # Labels[0] holds the value of the number of labels in the image
+        # Place a number in the top left indicating how many blossoms were detected
+        cv2.putText(visibleimg, str(labels[0]), (40,250), 1, 15, (0, 0, 255), 4)
         for x in range(labels[0]):
             # Convert coordinates to INT so they can be used in circle creation
             xcoord = int(centroids[x, 0])
@@ -80,7 +93,22 @@ class WindowClass(wx.Frame):
         # Create window to display our images in, set the window size to normal so it can display big images nicely
         cv2.namedWindow('Blossom Identifier', cv2.WINDOW_NORMAL)
         cv2.imshow('Blossom Identifier', visibleimg)
+        imagechoices = ['Red Band', 'Green Band', 'Blue Band', 'Red minus Green', 'Blue minus Green', 'Binary Image', 'Denoised Image']
+        imagelist = [r, g, b, rminusg, bminusg, binaryimg, denoiseimg]
+        imageselector = wx.Choice(self.panel, -1, pos=wx.Point(0, 115), choices=imagechoices, name='Image Selector')
+        self.Bind(wx.EVT_CHOICE, lambda event: self.showimage(event, imagelist[imageselector.GetSelection()]), imageselector)
 
+    def updatethreshold(self, e):
+        print('thres')
+        self.threshhold = int(self.panel.Children[2].GetValue())
+
+    def updatekernel(self, e):
+        print('kern')
+        self.kernelsize = int(self.panel.Children[3].GetValue())
+
+    def showimage(self, e, image):
+        cv2.namedWindow('Selected Image', cv2.WINDOW_NORMAL)
+        cv2.imshow('Selected Image', image)
 
 def main():
     app = wx.App()
--- test.py
+++ test.py
@@ -1,7 +1,71 @@
-import numpy as np
-import cv2
-#from matplotlib import pyplot as plt
+import wx
 
-img = cv2.imread('1_B_colorCanon_P_April10.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+class Mywin(wx.Frame):
+    def __init__(self, parent, title):
+        super(Mywin, self).__init__(parent, title=title, size=(350, 250))
+
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        hbox1 = wx.BoxSizer(wx.HORIZONTAL)
+        l1 = wx.StaticText(panel, -1, "Text Field")
+
+        hbox1.Add(l1, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.t1 = wx.TextCtrl(panel)
+
+        hbox1.Add(self.t1, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.t1.Bind(wx.EVT_TEXT, self.OnKeyTyped)
+        vbox.Add(hbox1)
+
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        l2 = wx.StaticText(panel, -1, "password field")
+
+        hbox2.Add(l2, 1, wx.ALIGN_LEFT | wx.ALL, 5)
+        self.t2 = wx.TextCtrl(panel, style=wx.TE_PASSWORD)
+        self.t2.SetMaxLength(5)
+
+        hbox2.Add(self.t2, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        vbox.Add(hbox2)
+        self.t2.Bind(wx.EVT_TEXT_MAXLEN, self.OnMaxLen)
+
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+        l3 = wx.StaticText(panel, -1, "Multiline Text")
+
+        hbox3.Add(l3, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.t3 = wx.TextCtrl(panel, size=(200, 100), style=wx.TE_MULTILINE)
+
+        hbox3.Add(self.t3, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        vbox.Add(hbox3)
+        self.t3.Bind(wx.EVT_TEXT_ENTER, self.OnEnterPressed)
+
+        hbox4 = wx.BoxSizer(wx.HORIZONTAL)
+        l4 = wx.StaticText(panel, -1, "Read only text")
+
+        hbox4.Add(l4, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        self.t4 = wx.TextCtrl(panel, value="ReadOnlyText",style = wx.TE_READONLY|wx.TE_CENTER)
+
+        hbox4.Add(self.t4, 1, wx.EXPAND | wx.ALIGN_LEFT | wx.ALL, 5)
+        vbox.Add(hbox4)
+        panel.SetSizer(vbox)
+
+        self.Centre()
+        self.Show()
+        self.Fit()
+
+    def OnKeyTyped(self, event):
+        print
+        event.GetString()
+
+    def OnEnterPressed(self, event):
+        print
+        "Enter pressed"
+
+    def OnMaxLen(self, event):
+        print
+        "Maximum length reached"
+
+
+app = wx.App()
+Mywin(None, 'TextCtrl demo')
+app.MainLoop()
